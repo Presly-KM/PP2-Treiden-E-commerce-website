59d1a327632bcd7adb6ddc21994b8970617f3197
Presly K.MALONGO <preslykomba.m@gmail.com>
2025-06-19 12:47:18 +0200

    add within reducer constant --> auth: authreducer

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,7 +1,9 @@
 import {configureStore} from '@reduxjs/toolkit'; // On importe configureStore depuis Redux Toolkit afin de permettre la création du store et ainsi pouvoir gérer l'état de l'application
-
+import authReducer from './slices/authSlice'; // On importe le reducer authReducer depuis le fichier authSlice.js. Ce reducer est utilisé pour gérer l'état de l'authentification de l'utilisateur dans l'application. Il contient les actions et les reducers nécessaires pour gérer la connexion, la déconnexion et l'état de l'utilisateur.
 const store = configureStore({  // On déclare une constante store qui va contenir le store de l'application. A l'intérieur de cette constante, on lui passe l'objet reducer qui contiendront les différents fonctions de chaque focntionnalité de l'application comme par exemple "user reducer" pour la gestion de l'utilisateur ou encore "cart reducer" pour la gestion du panier.
-    reducer: {}, // On initialise le store avec un objet vide pour l'instant, car nous n'avons pas encore de reducers définis.
+    reducer: {                // On initialise le store avec un objet vide pour l'instant, car nous n'avons pas encore de reducers définis.
+        auth: authReducer, // On ajoute le reducer authReducer au store sous la clé "auth". Cela permet de gérer l'état de l'authentification de l'utilisateur dans l'application.
+    }, 
 });
 
 
